a384a3ae51af8155cb8d9ff5bd0b40aa08148a22
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-02-18 16:10:48 -0600

    crate db engine

--- superset/db_engine_specs/crate.py
+++ superset/db_engine_specs/crate.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import logging
 from datetime import datetime
 from typing import Optional, TYPE_CHECKING
@@ -8,7 +25,7 @@ from superset.db_engine_specs.base import BaseEngineSpec
 logger = logging.getLogger()
 
 
-class CrateDBBaseEngineSpec(BaseEngineSpec):
+class CrateEngineSpec(BaseEngineSpec):
     """ Abstract class for Crate 'like' databases """
 
     engine = "crate"
@@ -29,8 +46,6 @@ class CrateDBBaseEngineSpec(BaseEngineSpec):
     @classmethod
     def convert_dttm(cls, target_type: str, dttm: datetime) -> Optional[str]:
         tt = target_type.upper()
-        if tt == utils.TemporalType.DATE:
-            return f"TO_DATE('{dttm.date().isoformat()}', 'YYYY-MM-DD')"
         if tt == utils.TemporalType.TIMESTAMP:
             dttm_formatted = dttm.isoformat(sep=" ", timespec="microseconds")
             return f"""DATE_FORMAT('{dttm_formatted}')"""
